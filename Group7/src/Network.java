e377214283d683900828532e6f71a9f8a9ea0760
aaron <aarongabor1@gmail.com>
2021-02-03 16:50:31 -0500

    Network class Version 1.1.0

--- Group7/src/Network.java
+++ Group7/src/Network.java
@@ -8,11 +8,11 @@
  *  	2 = Elevator Subsystem 
  * 
  * @author Aaron Gabor
- * @version 1.0.0
+ * @version 1.1.0
  */
 public class Network {
 	private FloorEvent floorEvent;
-	private boolean needToWait1, needToWait2, needToWait3, needToWait4; //Allows the correct method to be triggered
+	private boolean needToWait1, needToWait2, needToWait3, needToWait4, needToWait5; //Allows the correct method to be triggered
 	private int floor;
 	
 	/**
@@ -24,6 +24,7 @@ public class Network {
 		this.needToWait2 = false;
 		this.needToWait3 = false;
 		this.needToWait4 = false;
+		this.needToWait5 = false;
 	}
 	
 	/** 
@@ -37,7 +38,7 @@ public class Network {
 	public synchronized FloorEvent schedToFloorSystem(FloorEvent floorEvent, int systemNumber)
 	{
 		//Waits if this in not the method that should have been entered
-		while(this.needToWait2 || this.needToWait3 || this.needToWait4)
+		while(this.needToWait2 || this.needToWait3 || this.needToWait4 || this.needToWait5)
 		{
 			try
 			{
@@ -76,7 +77,7 @@ public class Network {
 	 */
 	public synchronized FloorEvent floorSystemToSched(FloorEvent floorEvent, int systemNumber)
 	{
-		while(this.needToWait1 || this.needToWait3 || this.needToWait4)
+		while(this.needToWait1 || this.needToWait3 || this.needToWait4 || this.needToWait5)
 		{
 			try
 			{
@@ -115,7 +116,7 @@ public class Network {
 	 */
 	public synchronized FloorEvent schedToElevatorSystem(FloorEvent floorEvent, int systemNumber)
 	{
-		while(this.needToWait1 || this.needToWait2 || this.needToWait4)
+		while(this.needToWait1 || this.needToWait2 || this.needToWait4 || this.needToWait5)
 		{
 			try
 			{
@@ -145,6 +146,45 @@ public class Network {
 	}
 	
 	/**
+	 * elevatorSystemToSched is a method that transfers a FloorEvent object from the Elevator
+	 * subsystem to the Scheduler subsystem.
+	 * 
+	 * @param floorEvent is the FloorEvent object that needs to be transfered
+	 * @param systemNumber the designated system number for the subsystem check key in class comment
+	 * @return Either the FloorEvent object that needs to be transfered or null
+	 */
+	public synchronized FloorEvent elevatorSystemToSched(FloorEvent floorEvent, int systemNumber)
+	{
+		while(this.needToWait1 || this.needToWait2 || this.needToWait3 || this.needToWait5)
+		{
+			try
+			{
+				wait();
+			}
+			catch(InterruptedException e)
+			{
+				System.err.print(e);
+			}
+		}
+		//Runs if the Elevator subsystem entered this method
+		if(systemNumber == 2)
+		{
+			this.needToWait4 = true;
+			this.floorEvent = floorEvent;
+			return null;
+		}
+		//Runs if the Scheduler subsystem entered this method
+		else if(systemNumber == 1)
+		{
+			this.needToWait4 = false;
+			FloorEvent newFloorEvent = this.floorEvent;
+			this.floorEvent = null;
+			return newFloorEvent;
+		}
+		return null;
+	}
+	
+	/**
 	 * floorToElevatorSystem is a method that transfers an floor number from the Floor subsystem to
 	 * the Elevator subsystem.
 	 * 
@@ -154,7 +194,7 @@ public class Network {
 	 */
 	public synchronized int floorToElevatorSystem(int floor, int systemNumber)
 	{
-		while(this.needToWait1 || this.needToWait2 || this.needToWait3)
+		while(this.needToWait1 || this.needToWait2 || this.needToWait3 || this.needToWait4)
 		{
 			try
 			{
@@ -168,14 +208,14 @@ public class Network {
 		//Runs if the Floor subsystem entered this method
 		if(systemNumber == 0)
 		{
-			this.needToWait4 = true;
+			this.needToWait5 = true;
 			this.floor = floor;
 			return -1;
 		}
 		//Runs if the Elevator subsystem entered this method
 		else if(systemNumber == 2)
 		{
-			this.needToWait4 = false;
+			this.needToWait5 = false;
 			int newFloorEvent = this.floor;
 			this.floor = -1;
 			return newFloorEvent;
