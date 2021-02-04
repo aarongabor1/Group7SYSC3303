332af76edbdd14ff9ced34689416aa6b605df2a6
dianamiraflor <dianamariemiraflor@cmail.carleton.ca>
2021-02-04 11:13:49 -0500

    Added methods in Elevator, ElevatorSubsystem, Motor and Scheduler.

--- Group7/src/Elevator.java
+++ Group7/src/Elevator.java
@@ -6,25 +6,42 @@ public class Elevator {
 	private Motor motor;
 	private Door door;
 	private int numberOfFloors;
+	private int currentFloor;
 	
 	public Elevator(int numberOfFloors) {
 		this.numberOfFloors = numberOfFloors;
 		this.elevatorButtons = new LinkedList<>();
+		this.motor = new Motor();
+		this.door = new Door(DoorPosition.CLOSED);
+		this.currentFloor = 1;
+		generateElevatorButtons();
 	}
 	
-	public void generateFloorButtons() {
+	public void generateElevatorButtons() {
 		for (int i = 1; i <= numberOfFloors; i++) {
 			elevatorButtons.add(new ElevatorButton(new ElevatorLamp(), i));
 		}
 	}
 	
-	public void pressDestinationButton(int destinationFloor) {
-		
+	public void moveDown() {
+		currentFloor--;
 	}
 	
-	public void
+	public void moveUp() {
+		currentFloor++;
+	}
 	
+	public int getCurrentFloor() {
+		return currentFloor;
+	}
 	
+	public Door getDoor() {
+		return door;
+	}
+	
+	public Motor getMotor() {
+		return motor;
+	}
 	
 	
 }
--- Group7/src/ElevatorSubsystem.java
+++ Group7/src/ElevatorSubsystem.java
@@ -1,37 +1,60 @@
 
 public class ElevatorSubsystem implements Runnable{
-	//private Network network;
+	private Network network;
+	private Scheduler scheduler;
 	private Elevator elevator;
-	private int currentFloor;
-	private boolean isMoving; // May be unnecessary because of elevator state enum
+	private int destinationFloor;
+	private Direction elevatorState; // May be unnecessary because of elevator state enum
 	
-	public ElevatorSubsystem(Elevator elevator) {
+	public ElevatorSubsystem(Elevator elevator, Scheduler scheduler, Network network) {
+		this.network = network;
 		this.elevator = elevator;
-		this.currentFloor = 1; // Get rid of magic numbers
+		this.scheduler = scheduler;
 	}
 
-	/**
-	 * Elevator subsystem receives the destination floor from FloorSubsystem
-	 */
 	@Override
 	public void run() {
-		while(currentFloor != ta)
+		while(!scheduler.isElevatorRequestsEmpty()) {
+			// int destinatonFloor = network.floorToElevator() should return the targeted floor
+			// int serviceFloor = 
+			// while (currentFloor!=destinationFloor)
+			// if currentFloor is above destinationFloor -> move elevator up, otherwise move down
+			// moveElevator()
+			// elevatorScheduler(currentFloor, elevatorState)
+			// if currentFloor is at destinationFloor -> stop motor, open doors	
+			
+			
+		}
 		
 	}
 	
-	public void operateMotor() {
-		elevator.
+	public int getDestinationFloor() {
+		return destinationFloor;
 	}
 	
-	public void handleMovingElevator() {
+	public void moveElevator(Direction direction) {
+		handleMotor(direction);
+		handleDoor(true);
 		
+		elevatorState = direction;
+		
+		if (direction == Direction.UP) {
+			elevator.moveUp();
+		} else if (direction == Direction.DOWN) {
+			elevator.moveDown();
+		}
 	}
 	
-	public int getCurrentFloor() {
-		return currentFloor;
+	public void handleMotor(Direction direction) {
+		elevator.getMotor().moveElevator(direction);
 	}
 	
-	public void setDestinationFloor(int destFloor) {
+	public void handleDoor(boolean isMoving) {
+		if (isMoving) {
+			elevator.getDoor().closeDoor();
+		} else {
+			elevator.getDoor().openDoor();
+		}
 		
 	}
 	
