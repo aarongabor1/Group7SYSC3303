36724f977ef9cccc00ebcf2886371f559c9b6234
dianamiraflor <dianamariemiraflor@cmail.carleton.ca>
2021-02-05 14:50:30 -0500

    Added javadocs

--- Group7/src/Elevator.java
+++ Group7/src/Elevator.java
@@ -1,6 +1,13 @@
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Elevator class is for the ElevatorSubsytem class to control.
+ * 
+ * @author Diana Miraflor
+ * @version 1.0
+ *
+ */
 public class Elevator {
 	public static final int MINIMUM_FLOOR_NUM = 1;
 	
@@ -10,6 +17,11 @@ public class Elevator {
 	private int numberOfFloors;
 	private int currentFloor;
 	
+	/**
+	 * Constructs a new Elevator object
+	 * 
+	 * @param numberOfFloors
+	 */
 	public Elevator(int numberOfFloors) {
 		this.numberOfFloors = numberOfFloors;
 		this.elevatorButtons = new LinkedList<>();
@@ -19,28 +31,48 @@ public class Elevator {
 		generateElevatorButtons();
 	}
 	
+	/**
+	 * Creates the elevator's floor buttons
+	 */
 	public void generateElevatorButtons() {
 		for (int i = 1; i <= numberOfFloors; i++) {
 			elevatorButtons.add(new ElevatorButton(new ElevatorLamp(), i));
 		}
 	}
 	
+	/**
+	 * Moves elevator down
+	 */
 	public void moveDown() {
 		currentFloor--;
 	}
 	
+	/**
+	 * Moves elevator up
+	 */
 	public void moveUp() {
 		currentFloor++;
 	}
 	
+	/**
+	 * Returns the current floor the elevator is at
+	 */
 	public int getCurrentFloor() {
 		return currentFloor;
 	}
 	
+	/**
+	 * Returns elevator's door
+	 * @return
+	 */
 	public Door getDoor() {
 		return door;
 	}
 	
+	/**
+	 * Retruns elevator's motor
+	 * @return
+	 */
 	public Motor getMotor() {
 		return motor;
 	}
